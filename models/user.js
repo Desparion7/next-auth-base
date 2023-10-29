dca5607989125db77cf3487c222ef4909eed95d0
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2023-10-29 11:10:34 +0100

    add user model, conection db function

---
+++ models/user.js
@@ -0,0 +1,50 @@
+import { Schema, model, models } from 'mongoose';
+import bcrypt from 'bcrypt';
+import { error } from 'console';
+
+const userSchema = new Schema({
+	email: {
+		type: String,
+		unique: [true, 'Email already exists!'],
+		required: [true, 'Email is required!'],
+	},
+	username: {
+		type: String,
+		required: [true, 'Username is required!'],
+	},
+	password: {
+		type: String,
+		require: true,
+	},
+	role: {
+		type: String,
+		enum: ['admin', 'user'],
+		default: 'user',
+	},
+	image: {
+		type: String,
+	},
+});
+// Hash the password before saving
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) return next;
+	try {
+		const salt = await bcrypt.genSalt(10);
+		this.password = bcrypt.hash(this.password, salt);
+		next();
+	} catch (error) {
+		throw error;
+	}
+});
+// Compare password method
+userSchema.methods.comparePassword = async function (password) {
+	try {
+		return await bcrypt.compare(password, this.password);
+	} catch (error) {
+		throw error;
+	}
+};
+
+const User = models.User || model('User', userSchema);
+
+export default User;
